2f71eada3ece24a7104a772c27fb41d28e38274d
caro1xxx <caro1xxxhv@gmail.com>
2022-01-05 16:45:12 +0800

    2022-01-05

---
+++ onclick和click的区别.js
@@ -0,0 +1,10 @@
+click:
+  click是HTML DOM Button对象的一个方法,也是HTML DOM click()方法,它是一个方法
+  需要拥有onclick 事件句柄
+onclick:
+  onclick是HTML DOM Event对象的事件 即它是一个事件
+  事件通常与函数结合使用，函数不会在事件发生前被执行
+
+终上所述可以得知就是事件和方法的区别:
+  事件名前一般都是on开头
+  方法是程序员编写调用,但是这个方法时要赋值给事件,有事件触发的
