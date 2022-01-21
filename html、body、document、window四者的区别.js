a1e8ba883864cab1f6a2378db0126e4ee432249d
caro1xxx <caro1xxxhv@gmail.com>
2022-01-21 16:48:23 +0800

    2022-01-21

---
+++ html、body、document、window四者的区别.js
@@ -0,0 +1,10 @@
+html:
+  有多重意义,既是超文本标记语言,也是html的根元素,其他元素都是子元素
+body:
+  body包含了全部的可视化内容,html显示的最终内容都是子元素,document.body访问
+document:
+  是window的子属性,用来访问页面中的元素,保存了操作DOM的api
+window:
+  window 对象在文档打开时自动创建，保存着 DOM，BOM，核心JS 等所有内容。
+  对于客户端 JS 而言，window 对象是其全局对象，通过访问 window 对象提供
+  的客户端 API 操作DOM，发起网络请求，进行本地存储，开启定时器，获取浏览器信息等各种能力。
