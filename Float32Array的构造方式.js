b40db7268db78377a9b47c4822dd0bcfc73d5e88
caro1xxx <caro1xxxhv@gmail.com>
2022-01-30 23:32:42 +0800

    2022-01-30

---
+++ Float32Array的构造方式.js
@@ -0,0 +1,6 @@
+详情见MDN:https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Float32Array
+以下四种构造方式:
+new Float32Array(length);
+new Float32Array(typedArray);
+new Float32Array(object);
+new Float32Array(buffer [, byteOffset [, length]]);
---
+++ TypedArray.js
@@ -0,0 +1,1 @@
+详情见MDN:https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/TypedArray
