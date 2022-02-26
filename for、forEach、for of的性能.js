5d8fbbf3eaaf8c6bda500878b5e573bdb306c73c
caro1xxx <caro1xxxhv@gmail.com>
2022-02-26 19:30:40 +0800

    2022-02-26

---
+++ for、forEach、for of的性能.js
@@ -0,0 +1,3 @@
+1.for 最好
+2.forEach与for of 相差无几 平均测试下来forEach略高for of
+3.forEach 无法通过 break跳出 for of内存占用上有一定的优势
---
+++ window.console.log和console.log区别.js
@@ -0,0 +1,5 @@
+不一样，window.console.log只能在浏览器中使用
+
+console.log在浏览器、Node等其他环境中都可以使用
+
+上下文环境不一样
