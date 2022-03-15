89487554b7653e00b5903c0c7158921b4c6c54b9
caro1xxx <caro1xxxhv@gmail.com>
2022-03-15 21:55:39 +0800

    2022-03-15

---
+++ 函数有哪几种调用形式.js
@@ -0,0 +1,9 @@
+正常函数调用
+fn()
+对象方法调用
+let obj = {fn:function(){}}
+obj.fn()
+构造函数调用
+new fn()
+call/apply调用
+fn.call()/fn.apply()
---
+++ 如何给1000个li绑定事件.js
@@ -0,0 +1,4 @@
+使用querySelector()//方法获取所有相同标签元素
+例子:
+  let l = document.querySelector('li') // 获取页面中所以的li
+  l.addEventListener(...)
